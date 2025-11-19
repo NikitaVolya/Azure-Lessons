0c831a2f1927cc63908bb32b59ad8884c813e50b
NikitaVoliaPC <volanskijnikita2@gmail.com>
2025-11-19 20:42:08 +0100

    Add project files.

---
+++ Lesson1/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Sas;
+
+namespace Lessons
+{
+    internal class Program
+    {
+        private static string localPath = "./data/";
+
+        static async Task Exercice1(BlobContainerClient containerClient)
+        {
+            string fileName = "test.txt";
+            string localFilePath = Path.Combine(localPath, fileName);
+
+            BlobClient blobClient = containerClient.GetBlobClient(fileName);
+
+            FileStream uplodaFileStream = File.OpenRead(localFilePath);
+            await blobClient.UploadAsync(uplodaFileStream, overwrite: true);
+            uplodaFileStream.Close();
+        }
+
+        static async Task Exercice2(BlobContainerClient containerClient)
+        {
+            string secondFileName = "second.txt";
+            string localFilePath = Path.Combine(localPath, secondFileName);
+            string blockName;
+
+            BlobClient blobClient = containerClient.GetBlobClient(secondFileName);
+            FileStream uplodaFileStream = File.OpenRead(localFilePath);
+
+            await blobClient.UploadAsync(uplodaFileStream, overwrite: true);
+            uplodaFileStream.Close();
+
+            Console.WriteLine("Chose one of files:");
+            foreach (BlobItem block in containerClient.GetBlobs())
+            {
+                Console.Write("{0}, ", block.Name);
+            }
+            Console.WriteLine();
+
+            blockName = Console.ReadLine();
+
+            BlobClient clien = containerClient.GetBlobClient(blockName);
+            BlobDownloadInfo download = await clien.DownloadAsync();
+
+            localFilePath = localFilePath.Replace(".", "DOWNLAOD.");
+            FileStream downloadFileStream = File.OpenWrite(localFilePath);
+            await download.Content.CopyToAsync(downloadFileStream);
+            downloadFileStream.Close();
+        }
+
+        static async Task Exercice3(BlobContainerClient containerClient)
+        {
+            string localFilePath, virtualFilePath;
+
+            string virtualPath = Path.Combine(
+                    DateTime.Now.Year.ToString(),
+                    DateTime.Now.Month.ToString(),
+                    DateTime.Now.DayOfWeek.ToString());
+            string[] pictures = { "picture1.jpg", "picture2.jpg", "picture3.jpg", "picture4.jpg" };
+
+            foreach (string fileName in pictures)
+            {
+                localFilePath = Path.Combine(localPath, fileName);
+                virtualFilePath = Path.Combine(virtualPath, fileName);
+
+                BlobClient blobClient = containerClient.GetBlobClient(virtualFilePath);
+
+                FileStream uplodaFileStream = File.OpenRead(localFilePath);
+                await blobClient.UploadAsync(uplodaFileStream, overwrite: true);
+                uplodaFileStream.Close();
+
+                BlobSasBuilder sas = new BlobSasBuilder()
+                {
+                    BlobContainerName = containerClient.Name,
+                    BlobName = virtualFilePath,
+                    Resource = "b",
+                    ExpiresOn = DateTime.UtcNow.AddHours(1)
+                };
+                sas.SetPermissions(BlobSasPermissions.Read);
+
+                Uri sasUri =containerClient.GetBlobClient(virtualFilePath).GenerateSasUri(sas);
+
+                Console.WriteLine(sasUri);
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            string connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+
+            string containerName = "testcontainer";
+
+            BlobServiceClient blobService = new BlobServiceClient(connectionString);
+
+            BlobContainerClient containerClient = new BlobContainerClient(connectionString, containerName);
+
+            await Exercice1(containerClient);
+            await Exercice2(containerClient);
+
+
+            BlobContainerClient picturesContainerClient = new BlobContainerClient(connectionString, "pictures");
+            await picturesContainerClient.CreateIfNotExistsAsync();
+
+            await Exercice3(picturesContainerClient);
+        }
+    }
+}
---
+++ Lesson1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lessons")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lessons")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f18474fb-7e9f-4fc4-8c31-dded6b487b02")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
